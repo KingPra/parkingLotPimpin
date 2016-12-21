6402cbb906c9b26fa619b24a65b1c405939deffd
KingPra <kingi.pra@gmail.com>
2016-12-21 18:20:25 -0500

    buttons working, waiting for post url

--- mustache.js
+++ mustache.js
@@ -4,88 +4,104 @@
 
 let cars = [
     {
-        'make': 'Acura',
-        'model': 'NSX',
-        'size': 3,
-        'funds': 20,
+        make: 'Acura',
+        model: 'NSX',
+        size: 3,
+        funds: 20,
     },
     {
-        'make': 'Honda',
-        'model': 'S2000',
-        'size': 2,
-        'funds': 78,
+        make: 'Honda',
+        model: 'S2000',
+        size: 2,
+        funds: 78,
     },
     {
-        'make': 'Toyota',
-        'model': 'Supra TT',
-        'size': 4,
-        'funds': 45,
+        make: 'Toyota',
+        model: 'Supra TT',
+        size: 4,
+        funds: 45,
     },
     {
-        'make': 'Honda',
-        'model': 'Civic Type R',
-        'size': 2,
-        'funds': 145,
+        make: 'Honda',
+        model: 'Civic Type R',
+        size: 2,
+        funds: 145,
     },
     {
-        'make': 'Jeep',
-        'model': 'Wrangler Unlimited Rubicon',
-        'size': 6,
-        'funds': 35,
+        make: 'Jeep',
+        model: 'Wrangler Unlimited Rubicon',
+        size: 6,
+        funds: 35,
     },
     {
-        'make': 'Jeep',
-        'model': 'Renegade Trailhawk',
-        'size': 4,
-        'funds': 87,
+        make: 'Jeep',
+        model: 'Renegade Trailhawk',
+        size: 4,
+        funds: 87,
     },
     {
-        'make': 'Toyota',
-        'model': 'Tundra TRD',
-        'size': 7,
-        'funds': 25,
+        make: 'Toyota',
+        model: 'Tundra TRD',
+        size: 7,
+        funds: 25,
     },
     {
-        'make': 'Ford',
-        'model': 'Raptor 6.2L, not the 5.4L',
-        'size': 7,
-        'funds': 34,
+        make: 'Ford',
+        model: 'Raptor 6.2L, not the 5.4L',
+        size: 7,
+        funds: 34,
     },
 ]
 
 
-let parkingLots =
-    [
-        {
-            'id': 0,
-            'capacity': 5,
-            'maxCapacity': 15,
-            'rate': 5,
-        },
-        {
-            'id': 1,
-            'capacity': 5,
-            'maxCapacity': 15,
-            'rate': 5,
-        },
-        {
-            'id': 2,
-            'capacity': 5,
-            'maxCapacity': 15,
-            'rate': 5,
-        },
-        {
-            'id': 3,
-            'capacity': 5,
-            'maxCapacity': 15,
-            'rate': 5,
-        },
-    ]
-
 // event listener. after page loads, function is called
 window.addEventListener('load', function () {
 
-    // for loop for car array
+let request = new XMLHttpRequest();
+request.open('GET', 'https://mysterious-wildwood-46789.herokuapp.com/lots');
+request.addEventListener('load', function () {
+    let response = JSON.parse(request.responseText);  
+    console.log('loaded') 
+
+
+        // for loop for parking lot array
+    for (let i = 0; i < response.length; i++) {
+        // creating a new li
+        let lotList = document.createElement('li');
+        // links lotList to Mustache method in html
+        lotList.innerHTML = Mustache.render(
+            document.querySelector('#lots').innerHTML,
+            {//html: js array   
+                lot: response[i].id,
+                capacity: response[i].capacity,
+                cost: response[i].rate
+            }
+        );
+        let adopter = document.querySelector('.parking-lot');
+        adopter.appendChild(lotList);
+        console.log('lotList is running');
+        console.log(response[i].id);
+    }
+});
+request.send();
+
+carList(cars);
+
+
+
+
+
+
+
+//lotButtonOne(response[0].id);
+
+
+
+});
+
+
+function carList() {
+        // for loop for car array
     for (let i = 0; i < cars.length; i++) {
         // creating a new li
         let carList = document.createElement('li');
@@ -106,63 +122,64 @@ window.addEventListener('load', function () {
                 ]}
             
         );
+
+        //lexi code
+let buttonOne = carList.querySelector('.plots-0').addEventListener('click', function () {
+    updateCars(0, cars[i]);
+    console.log('this is first number' + 0, cars[i]);
+});
+let buttonTwo = carList.querySelector('.plots-1').addEventListener('click', function () {
+    updateCars(1, cars[i]);
+});
+let buttonThree = carList.querySelector('.plots-2').addEventListener('click', function () {
+    updateCars(2, cars[i]);
+});
+let buttonFour = carList.querySelector('.plots-3').addEventListener('click', function () {
+    updateCars(3, cars[i]);
+});
         // parent variable to class car-list
         let parent = document.querySelector('.car-list');
         //"parent" adopts carList
         parent.appendChild(carList);
         //console.log(buttonCount);
-    }
-    
 
-
-    /**
-     * buttons 
-     * 1. loop through cars
-     * 2. loop through buttons
-     * 3. assign lot and assign car to buttons
-     * 4. button sends car to lot.
-     */
-   // for (i = 0; i < cars.length; i++) {
-     //   for (j = 0; j < parkingLots. length; j++) {
-
-       //         let buttonList = document.createElement('button');
-                // buttonList.innerHTML = Mustache.render(
-                //     document.querySelector('#cars').innerHTML,
-                //     {
-                //         id: parkingLots[i].id,
-                //     }
-                // );
-             //   let buttonDaddy = document.querySelector('.car-list');
-             //   buttonDaddy.appendChild(buttonList);
-         //       console.log('button list should be in full effect');
-                //document.addEventListener('click', button);
-   //         }
-     //   }
-    
-
-
-
-
-    // for loop for parking lot array
-    for (let i = 0; i < parkingLots.length; i++) {
-        // creating a new li
-        let lotList = document.createElement('li');
-        // links lotList to Mustache method in html
-        lotList.innerHTML = Mustache.render(
-            document.querySelector('#lots').innerHTML,
-            {//html: js array   
-                lot: parkingLots[i].id,
-                capacity: parkingLots[i].capacity + '/' + parkingLots[i].maxCapacity,
-                rate: parkingLots[i].rate,
-            }
-        );
-        let adopter = document.querySelector('.parking-lot');
-        adopter.appendChild(lotList);
-        console.log('lotList is running');
-        console.log(parkingLots[i].id);
     }
 
+}
 
 
-});
+//lexi code 
+function updateCars (lotNum, car) {
 
+    let newParkedCar = {
+        lotNum: lotNum,
+        make: cars.make,
+        model: cars.model, 
+        size: cars.size,
+        funds: cars.funds,
+    }
+    let request = new XMLHttpRequest();
+    request.open('POST', 'https://mysterious-wildwood-46789.herokuapp.com/lots');
+    request.send(JSON.stringify(updateCars));
+    console.log('updateCars is running');
+}
+
+// function lotButtonOne () {
+//     response[0].capacity++;
+// console.log('lot0 ' + response[0].capacity);
+// }
+
+// function lotButtonTwo () {
+//     response[1].capacity++;
+// console.log('lot1: ' + response[1].capacity);
+// }
+
+// function lotButtonThree () {
+//     response[2].capacity++;
+// console.log('lot2 ' + response[2].capacity);
+// }
+
+// function lotButtonFour () {
+//     response[3].capacity++;
+// console.log('lot3: ' + response[3].capacity);
+// }
